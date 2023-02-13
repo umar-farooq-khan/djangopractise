de6539cd354cf4ddc7cf56da77c70554d200e6db
Umar Farooq khan <umaR.farooq407@gmail.com>
2023-02-13 16:23:07 +0530

    careers list added
    Added the career list in the bottom of the page but without functionality

--- OCR/insta reel download innsit.py
+++ OCR/insta reel download innsit.py
@@ -2,7 +2,7 @@ import requests
 from datetime import date
 today = date.today()
 
-file= open(r"C:\Users\umaRf\OneDrive\Desktop\Transfer\20230209T073307+0500_ExportTabsURLs.txt").read()
+file= open(r"urlss.txt").read()
 lines =file.split('\n')
 x=0
 for url in lines:
@@ -10,5 +10,6 @@ for url in lines:
     if(url.__contains__('cdninstagram') or url.__contains__('fbcdn') ):
         r = requests.get(url, allow_redirects=True)
         d4 = today.strftime("%b-%d-%Y")
-        open(str(x)+ d4+ '.mp4', 'wb').write(r.content)
+        open(r'C:/Insta Downloaded Videos Reel/' + str(x)+ d4+ '.mp4', 'wb').write(r.content)
         x=x+1
+        # 6 nov
