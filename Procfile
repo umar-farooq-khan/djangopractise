7ac2a2d3f833cee8a4d259e8493fe91cf7248706
Umar Farooq khan <umaR.farooq407@gmail.com>
2023-03-25 22:36:49 +0200

    railway

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn djangopractise-wsgi --log file -
--- djangopractise/settings.py
+++ djangopractise/settings.py
@@ -140,8 +140,8 @@ USE_I18N = True
 
 USE_TZ = True
 
-#ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = ['192.168.100.1', 'localhost', '127.0.0.1:8000']
+ALLOWED_HOSTS = ['*']
+#ALLOWED_HOSTS = ['192.168.100.1', 'localhost', '127.0.0.1:8000']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
